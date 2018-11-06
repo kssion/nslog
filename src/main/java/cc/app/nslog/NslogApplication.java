e50edb0ad41198f2cebb5843110ec12b4366c238
Rencp <kssion@yeah.net>
2018-11-07 01:01:58 +0800

    First Commit

---
+++ src/main/java/cc/app/nslog/NslogApplication.java
@@ -0,0 +1,12 @@
+package cc.app.nslog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NslogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NslogApplication.class, args);
+    }
+}
